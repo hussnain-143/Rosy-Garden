522e79b5137dd73abb6cad859488211cc515f018
Hussnain Ahmed <143833798+hussnain-143@users.noreply.github.com>
2025-09-06 21:31:28 +0500

    set the ui components (header, footer , toast message, button, loader)

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Components/layouts/Header";
+import Footer from "../Components/layouts/Footer";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="flex mx-auto flex-col min-h-screen">
+      {/* Header */}
+      <div className="h-20">
+        <Header />
+      </div>
+      <div className="none mt-4 md:block border-2 container max-w-[1200px]  mx-auto"></div>
 
-export default Layout
+      {/* Main Content */}
+      <main className="flex-1 h-[80vh]">
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <div className="bg-gray-200 p-3">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
